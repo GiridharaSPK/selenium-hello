20c2fdfb1bbe6fcec5a0df1a04dfdfbf5a1db96e
giridhara.gurram <giridhara.gurram@addverb.in>
2021-03-02 00:04:38 +0530

    xpath

--- test2.py
+++ test2.py
@@ -6,7 +6,8 @@ driver = webdriver.Chrome('./chromedriver')
 
 driver.get('https://www.google.com')
 
-input = driver.find_element_by_name('q')
+# input = driver.find_element_by_name('q')
+input = driver.find_element_by_xpath("/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input")
 input.send_keys("hello world"+Keys.RETURN)
 
 logo = driver.find_element_by_id('logo')
